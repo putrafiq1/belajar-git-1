6b4b849083b2f0e6d356c5a8cef9cb5d40a6360c
putrafiq1 <pikiyolputra@gmail.com>
2023-06-08 02:52:06 +0700

    Membuat main file & fungsi main_menu

---
+++ menu_kasir_sederhana.py
@@ -0,0 +1,30 @@
+import os 
+
+def menu():
+  os.system('cls')
+  while True:
+    print('='*30)
+    print('MENU KASIR SEDERHANA'.center(30))
+    print('='*30)
+    print('1. Input Data Barang')
+    print('2. Input Data Konsumen')
+    print('0. Exit')
+    print('='*30)
+    try:
+      pilihan = int(input('Pilih Menu: '))
+      if pilihan == 1:
+        pass
+      elif pilihan == 2:
+        pass
+      elif pilihan == 0:
+        os.system('cls')
+        print('='*30)
+        print('Terima kasir Telah Menggunakan Program'.center(30))
+        print('='*30)
+        return
+      else:
+        os.system('cls')
+        print(f'Tidak Ada Menu Ke-{pilihan}'.center(30))
+    except ValueError:
+      os.system('cls')
+      print('!! Input Harus Angka !!'.center(30))
